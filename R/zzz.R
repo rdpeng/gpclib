33ad53ff363fcaab6b84500765fe525d7171771b
Roger D. Peng [amelia] <rdpeng@gmail.com>
2012-03-21 15:07:13 -0400

    Don't load packages in .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-.onLoad <- function(lib, pkg) {
-    if(!require(methods, quietly = TRUE))
-        stop("'methods' package is required for 'gpclib'")
-}
-
 .onAttach <- function(lib, pkg) {
     ver <- read.dcf(file.path(lib,pkg,"DESCRIPTION"), "Version")
     msg <- sprintf("General Polygon Clipper Library for R (version %s)\n\tType 'class ? gpc.poly' for help\n", as.character(ver))
