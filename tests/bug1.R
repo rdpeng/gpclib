dc28597362f0d383ebbde1ad2ae7529d743956c2
Roger D. Peng <rdpeng@jhu.edu>
2020-02-07 11:38:45 -0500

    Need to use ::: for unexported C functions

--- tests/bug1.R
+++ tests/bug1.R
@@ -6,5 +6,5 @@ plot(as(vec, "gpc.poly"))  ## looks fine
 clip <- to_numeric(p); str(clip)
 
 ## Crashes R 2.14 and 2.15 (32-bit) on Windows
-vec <- .Call("Rgpc_polygon_clip", vec, clip, 3, PACKAGE="gpclib")
+vec <- .Call(gpclib:::C_Rgpc_polygon_clip, vec, clip, 3)
 str(vec)
